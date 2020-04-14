9df9b44d8cc9135b86e536406f227f1de17f67c6
Aditi <adidhamangaonkar@gmail.com>
2020-04-15 03:57:51 +0530

    first commmit

---
+++ handler.js
@@ -0,0 +1,19 @@
+'use strict';
+
+module.exports.calSum = (event, context, callback)  => {
+  let result = 0;
+  let n = 10;
+  for(let i = 1; i <= n; i++) {
+    result = result + i;
+  }
+     
+ const response = {
+   statusCode: 200,
+   body: JSON.stringify({
+     message: 'OK',
+       total: result
+   }),
+ };
+ 
+  callback(null, response)
+}
